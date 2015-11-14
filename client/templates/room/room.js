56d81625cf3f1579fd1d4e757a6e6a633786023d
Anthony Liu <anthonyscottliu@gmail.com>
2015-11-14 03:24:52 -0500

    Changes the room id

--- client/templates/room/room.js
+++ client/templates/room/room.js
@@ -1,29 +1,36 @@
 function handleAR(roomId) {
+    console.log('in handleAR ' + new Date());
+
     var comm = new Icecomm('3kB4PpZaNNFN4r3xhmOVgcPn2D8rzcOTtQFh4gRwmAsaGTPwlm');
     comm.connect(roomId, {audio: false});
 
     comm.on('connected', function(peer) {
-        video2.src=peer.stream;
+        v2.src = peer.stream;
     });
 
     comm.on('local', function(peer) {
-        video.src = peer.stream;
+        v.src = peer.stream;
     });
 
     var v = document.getElementById("video");
     var v2 = document.getElementById("video2");
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    var i;
-    var i2;
+    var i, i2;
 
-    v.addEventListener("loadeddata", function() {i = window.setInterval(function() {},20);}, false);
-    v2.addEventListener("loadeddata", function() {i2 = window.setInterval(function() {
-    ctx.drawImage(v,5,5,500,500);
-    ctx.globalAlpha = 0.5;
-    ctx.drawImage(v2,5,5,500,500);},20);}, false);
+    v.addEventListener("loadeddata", function() {
+        i = window.setInterval(function(){}, 20);
+    }, false);
+    v2.addEventListener("loadeddata", function() {
+        i2 = window.setInterval(function() {
+            ctx.drawImage(v,5,5,500,500);
+            ctx.globalAlpha = 0.5;
+            ctx.drawImage(v2,5,5,500,500);
+        }, 20);
+    }, false);
 };
 
 Template.room.onRendered(function() {
-    handleAR(Router.current().params._id);
+    //handleAR(Router.current().params._id);
+    handleAR('custom room');
 });
